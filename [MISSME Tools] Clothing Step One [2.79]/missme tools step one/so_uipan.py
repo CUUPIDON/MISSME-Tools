54739a4f5f184806bb74553963eb5ccc90d5d910
MISSME12 <73448507+MISSME12@users.noreply.github.com>
2021-04-23 19:32:58 -0400

    Add files via upload

---
+++ [MISSME Tools] Clothing Step One [2.79]/missme tools step one/so_uipan.py
@@ -0,0 +1,76 @@
+import bpy
+
+class MissMePanel(bpy.types.Panel):
+    """Creates a Panel in the Tool Shelf"""
+    bl_label = "MISSME Tools: Step One"
+    bl_idname = "OBJECT_PT_missme"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    bl_category = "MISSME"
+
+    def draw(self, context):
+        layout = self.layout
+
+        obj = context.object
+        
+        row = layout.row()
+        row.label(text="A PY script that does most of the hard-work for you.", icon= 'INFO') 
+        row = layout.row()
+        row.label(text="Arrange the UVs for your MD Mesh and rename the UVMap to UV_0 then join it to s4studio_mesh_1.", icon= 'INFO') 
+        row = layout.row()
+        row.label(text="Make sure you import your reference mesh and name it REF so the script can find the object it needs.", icon= 'ERROR')
+        row = layout.row()
+        row.label(text="When you've done that, click Run Script at the bottom." , icon= 'INFO')
+
+#Data Transfer
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()
+        row.label(text="Smooth and Transfers UV_1 to your mesh:")
+
+        row = layout.row()
+        row.operator("datatransfer.operator", text='Data Transfer')
+        
+#Finished?                  
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()       
+        row = layout.row()          
+        row = layout.row()
+        row.label(text="Step one done!", icon= 'INFO') 
+        row = layout.row()
+        row.label(text="Check and make sure the UV_1 was properly transferred!", icon= 'ERROR') 
+        row = layout.row()            
+        row.label(text="When you're ready, open Blender 2.70 and continue to step two.", icon= 'INFO')
+        
+#Patreon Button
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()
+        row.label(text="If you like my work, consider supporting me on Patreon:", icon='SOLO_ON')
+        
+        row = layout.row()
+        row.operator('wm.url_open', text='MISSME').url='https://www.patreon.com/missme'
+        
+        
+        
+
+
+
+def register():
+    bpy.utils.register_class(MissMePanel)
+
+
+def unregister():
+    bpy.utils.unregister_class(MissMePanel)
+
+
+if __name__ == "__main__":
+    register()
